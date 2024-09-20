1c1c28f8a785bbac6c33760a1ead286ea20ec299
Engr. Safa Anwar <110125728+safaanwar1122@users.noreply.github.com>
2024-09-20 20:29:54 +0500

    Time: 0 ms (100%), Space: 8.2 MB (87.87%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int start=0;
+        int end=x;
+      int  mid,ans;
+        if(x<2){return x;}
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(mid==x/mid)
+            {
+                return mid;
+            }
+            else if(mid<x/mid)
+            {
+                ans=mid;
+                start=mid+1;
+            }
+            else {
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+};
